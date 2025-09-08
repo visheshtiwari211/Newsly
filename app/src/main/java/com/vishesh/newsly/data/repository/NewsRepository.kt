036c232da7fd924b2ff11b230c26b77d09eefd63
visheshtiwari <visheshtiwari211@gmail.com>
2025-09-08 21:28:48 +0530

    feat(newsrepository): created repository layer

---
+++ app/src/main/java/com/vishesh/newsly/data/repository/NewsRepository.kt
@@ -0,0 +1,9 @@
+package com.vishesh.newsly.data.repository
+
+import com.vishesh.newsly.data.api.NewsApi
+import javax.inject.Inject
+
+class NewsRepository @Inject constructor(private val newsApi: NewsApi) {
+    suspend fun getTopHeadline(country: String, category: String, apiKey: String) =
+        newsApi.getTopHeadlines(country = country, category = category, apiKey = apiKey)
+}
