b1ebe7f424c5db25d19f438877259d4397c76927
visheshtiwari <visheshtiwari211@gmail.com>
2025-09-08 19:53:06 +0530

    feat(api): setup the api interface for retrofit

---
+++ app/src/main/java/com/vishesh/newsly/data/api/NewsApi.kt
@@ -0,0 +1,14 @@
+package com.vishesh.newsly.data.api
+
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface NewsApi {
+    @GET("top-headlines")
+    suspend fun getTopHeadlines(
+        @Query("country") country: String,
+        @Query("category") category: String,
+        @Query("apiKey") apiKey: String
+    ): Response<Any>
+}
