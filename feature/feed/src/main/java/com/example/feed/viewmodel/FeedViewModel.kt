e78cddeb630b722fd8a24c79caf6279c26f3b112
visheshtiwari <visheshtiwari211@gmail.com>
2025-09-24 23:57:16 +0530

    feat(favorite): added favorite functionality in viewmodel

--- feature/feed/src/main/java/com/example/feed/viewmodel/FeedViewModel.kt
+++ feature/feed/src/main/java/com/example/feed/viewmodel/FeedViewModel.kt
@@ -8,6 +8,7 @@ import com.example.data.repository.NewsRepository
 import com.example.model.model.Article
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
@@ -15,5 +16,11 @@ class FeedViewModel @Inject constructor(private val repository: NewsRepository) 
     val pagedArticle: Flow<PagingData<Article>> =
         repository.getPagedArticles(country = "us", apiKey = "35e96e729a6848a48c1c31aaf412b671")
             .cachedIn(viewModelScope)
+
+    fun setIsFavoriteArticle(url: String, isFavorite: Boolean) {
+        viewModelScope.launch {
+            repository.setFavoriteArticles(url = url, isFavorite = isFavorite)
+        }
+    }
 
 }
