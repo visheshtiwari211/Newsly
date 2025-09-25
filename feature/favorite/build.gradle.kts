6eb65aae818c21f1dc8d1d4c4c6bd2fba196b43e
visheshtiwari <visheshtiwari211@gmail.com>
2025-09-25 23:51:16 +0530

    feat(build): added dependencies in fav gradle

--- feature/favorite/build.gradle.kts
+++ feature/favorite/build.gradle.kts
@@ -1,6 +1,9 @@
 plugins {
     alias(libs.plugins.android.library)
     alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.kotlin.compose)
+    alias(libs.plugins.ksp)
+    alias(libs.plugins.hilt)
 }
 
 android {
@@ -33,11 +36,25 @@ android {
 }
 
 dependencies {
-
+    implementation(project(":data"))
+    implementation(project(":model"))
+    implementation(project(":common"))
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
+    implementation(libs.androidx.compose.foundation)
+    implementation(libs.androidx.material3)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+    implementation(libs.androidx.compose.ui)
+    implementation(libs.androidx.compose.ui.graphics)
+    implementation(libs.androidx.compose.ui.tooling.preview)
+
+    //hilt-dependencies
+    implementation(libs.hilt.android)
+    ksp(libs.hilt.compiler)
+
+    //hilt-navigation
+    implementation(libs.androidx.hilt.navigation.compose)
 }
