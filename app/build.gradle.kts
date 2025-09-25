f363992f738b0aeafe55d7abc2954e7ca87993bf
visheshtiwari <visheshtiwari211@gmail.com>
2025-09-26 00:52:37 +0530

    feat(navigation): added fav screen navigation

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -48,6 +48,7 @@ android {
 dependencies {
     implementation(project(":feature:feed"))
     implementation(project(":feature:detail"))
+    implementation(project(":feature:favorite"))
     
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
